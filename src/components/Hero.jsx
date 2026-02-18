1b3b40613f439988e6d98dc4a1c3a71a435cd7dd
Tawakalt10 <raheemtawa@gmail.comm>
2026-02-18 16:29:42 +0100

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,42 @@
+import {useEffect, useState } from "react";
+
 export default function Hero () {
-    return <div>
-        
-    </div>;
+
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    
+
+      useEffect(() => {
+        function handleMouseMove(e) {
+
+      setMousePosition({x: e.clientX, y: e.clientY})
+        }
+        window.addEventListener("mousemove", handleMouseMove)
+        return () =>  window.removeEventListener("mousemove", handleMouseMove)
+      }, []);
+
+      return ( 
+     <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 lg:px-8 overflow-hidden">
+      <div className="absolute inset-0 opacity-30"  
+      style={{
+      background: `radial-gradient(600px= circle at ${mousePosition.x}px ${mousePosition.y}px, rgba(59, 130, 246, 0.15), transparent 40%)`
+      }}
+      />
+      <div className="absolute top-20 left-4 sm:left-10 w-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse"></div>
+      <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm::w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
+  
+      <div className="relative order-2 w-full">
+           <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl px-3 sm:p-4 shadow-2xl borde border-white/10">
+           <div className="bg-gradient-to-bg from-gray-900/20 to gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[200px] sm:w-[350px] lg:h-[450px] border border-white/5">
+            {/*  IDE HEADER */}
+            <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm boder-b border-white/10"></div>
+        </div>
+      </div>
+
+      </div>
+     
+  
+    </section>
+    
+    );
+    
 }    
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import {Menu} from "lucide-react"
+import {Menu, X} from "lucide-react"
+import { useState } from "react";
 
 export default function Navbar () {
+     const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
+
     return <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-red-950/20 backdrop-blur-sm ">
 
       <div className="max-w-7xl mx-auto px-4 lg:px-8">
@@ -17,7 +20,7 @@ export default function Navbar () {
       </div>
 
       {/* Nav list */}
-      <div className="flex items-center space-x-6 lg:space-x-8">
+      <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
         <a href="#features" className="text-gray-300 hover:text-white text-sm lg:tex-base">
              Features
         </a>
@@ -30,12 +33,41 @@ export default function Navbar () {
              Testiomonials
         </a>
       </div>
-       <button>
-        <Menu/>
+       <button className="md:hidden p-2 text-gray-300 hover:text-white"
+        onClick={() => setMobileMenuIsOpen ((prev) =>!prev )}>
+
+        {mobileMenuIsOpen  ? ( <X className="w-5 h-5 sm:h-6 "/> ) : (
+        <Menu className="w-5 h-5 sm:h-6 " />
+        )}
        </button>
 
        </div>
       </div>
+
+      {mobileMenuIsOpen && (
+        <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in slide-in-from-top ">
+         <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4">
+            <a href="#features" 
+             onClick={() => setMobileMenuIsOpen ((prev) =>!prev )}
+            className= "block text-gray-300 hover:text-white text-sm lg:tex-base">
+             Features
+        </a>
+        
+           <a href="#pricing" 
+            onClick={() => setMobileMenuIsOpen ((prev) =>!prev )}
+           className=" block text-gray-300 hover:text-white text-sm lg:tex-base">
+             Pricing
+        </a>
+
+           <a href="#testimonials"
+            onClick={() => setMobileMenuIsOpen ((prev) =>!prev )}
+           className=" block text-gray-300 hover:text-white text-sm lg:tex-base">
+             Testiomonials
+        </a>
+         </div>
+        
+        </div>
+        )}
   
     </nav>;
 }    
