713402c424d7a3064cb0754b765538872d226ef2
Tawakalt10 <raheemtawa@gmail.comm>
2026-04-03 13:36:44 +0100

    new

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -30,7 +30,7 @@ const features = [
 
 {
    title: "Smart debugging",
-   description: "Identify and fix bugs before they reach production. AI-powered error debugging",
+   description: "Identify and fix bugs before they reach production. AI-powered error detection and resolution suggestion help you debug faster",
    codeSnippet: `function handleSubmit() {
 //    AI suggests next line
 // const data= await fetch ("/api/submit")}
@@ -45,7 +45,7 @@ const features = [
 export default function Features () {
     return (
          <section id="features" className="py-16 sm:py-20 px-10 sm:px-6 lg:px-8 relative">
-        <div>
+        <div className="max-w-6xl mx-auto">
        <div className="text-center mb-12 sm:mb-16  lg:mb-20"> 
         <h2 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 sm:mb-6 ">
              <span className="bg-gradient-to-b from-white to-gray-300 bg-clip-text text-transparent"> Your Complete Development</span>
@@ -59,15 +59,19 @@ export default function Features () {
 
         {features.map((feature, key )=> (
             <div key={key} 
-            className={`flex flex-col lg:flex-grow items-center gap-8 sm:gap-12 ${
+            className={`flex flex-col lg:flex-row items-center gap-8 sm:gap-12 ${
             feature.imagePosition === "right" ? "lg:flex-row-reverse" : ""}` }>
             {/* Code Section */}
             <div className="flex-1 w-full">
                 <div className="relative group ">
+                    <div className="absolute inset-0 bg-gradient-to-br from-blue-500/20 to-purple-500/20
+                    rounded-xl sm:rounded-2xl transition-all duration-500"
+                     />
+                    
                 <div className="relative bg-gray-900/50 backdrop-blur-sm border border-gray-700/50
                 rounded-xl sm:rounded-2xl p-4 sm:p-6 overflow-hidden group-hover:border-1
                 group-hover:border-blue-600/50 transition-all duration-300">
-                {/* Ide interview */}
+                {/* Ide interface */}
                 <div className=" bg-gray-950 rounded-lg  p-3 sm:p-4 font-mono text-xs sm:text-sm">
                     <div className="flex items-center space-x-1 sm:space-x-2 mb-3 sm:mb-4">
              <div  className=" flex items-center space-x-1 sm:space-x-2">
@@ -83,6 +87,7 @@ export default function Features () {
                  <div>
                  <SyntaxHighlighter language="javascript" style={nightOwl} customStyle={{
                           margin: 0, 
+                          background: "transparent",
                           borderRadius:
                            '8px', 
                            fontSize:'0.75rem',
